dd81b11c6525b13f8526fd5655e7b82bf01739f6
Rachit Jain <220160212042.rachit@gdgu.org>
2024-07-10 12:12:28 +0530

    Update student.py

--- student.py
+++ student.py
@@ -33,6 +33,24 @@ class Student:
     f_lbl = Label(self.root, image = self.photoimg2)
     f_lbl.place(x=1280, y=0, width=640, height=200)
 
+    #  BG Image
+    img3 = Image.open(r"Images/GD Goenka University.jpg")
+    img3 = img3.resize((1920, 880), Image.LANCZOS)
+    self.photoimg3 = ImageTk.PhotoImage(img3)
+
+    bg_img = Label(self.root, image = self.photoimg3)
+    bg_img.place(x=0, y=200, width=1920, height=880)
+
+    title_lbl = Label(bg_img, text="STUDENT MANAGEMENT SYSTEM", font=("times new roman", 35, "bold"), bg="blue", fg="red")
+    title_lbl.place(x=0, y=0, width=1920, height=50)
+
+    # Main Frame
+    main_frame = Frame(bg_img, bd=2, bg="pink")
+    main_frame.place(x=10, y=60, width=1900, height=750)
+
+    # Left Frame
+    left_Frame = LabelFrame(main_frame, bd=2, relief="ridge", text="Student Details", font=("times new roman", 12, "bold") )
+    left_Frame.place(x=20, y=10, width=940, height=700)
 
 def main() -> None:
   root = Tk()
