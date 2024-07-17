dae4251d64b90939dc3b2e58f9faedcb3a09e95c
Rachit Jain <220160212042.rachit@gdgu.org>
2024-07-18 02:25:22 +0530

    Update student.py

--- student.py
+++ student.py
@@ -344,11 +344,10 @@ class Student:
     my_cursor = conn.cursor()
     my_cursor.execute("select * from student")
     data = my_cursor.fetchall()
-    if len(data) != 0:
-      self.student_table.delete(*self.student_table.get_children())
-      for i in data:
-        self.student_table.insert("",END,values=i)
-        conn.commit()
+    self.student_table.delete(*self.student_table.get_children())
+    for i in data:
+      self.student_table.insert("",END,values=i)
+      conn.commit()
     conn.close()
   
   # ================ Get Cursor ================
