206367b869eb784784da73fa6b27297ead1f9607
Rhythm280 <119752752+Rhythm280@users.noreply.github.com>
2024-07-22 01:24:07 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,10 +5,10 @@ import mysql.connector
 import cv2
 import os
 import numpy as np
+import face_recognition
 
 
 class Train:
-  
   def __init__(self, root) -> None:
         self.root = root
         self.root.geometry("1920x1080+0+0")
@@ -30,7 +30,7 @@ class Train:
         # ============= Train_Button =================
 
         b1_1 = Button(self.root, text="TRAIN DATA", cursor="hand2", command=self.train_clasifier, font=("times new roman", 20, "bold"), bg="darkblue", fg="white")
-        b1_1.place(x=0, y=350, width=1980, height=50)
+        b1_1.place(x=0, y=350, width=1920, height=50)
 
         # ===========Bottom Image ===================
         img_bottom = Image.open(r"Images/crowd1.jfif")  #change the image
@@ -50,13 +50,13 @@ class Train:
      for image in path:
         img=Image.open(image).convert('L')  # Gray scale image
         imageNp=np.array(img, 'uint8')
-        id=int(os.path.split(image)[1].split('.')[1])
+        id=int(os.path.split(image)[1].split('_')[0])
 
 
         faces.append(imageNp)
         ids.append(id)
         cv2.imshow("Training", imageNp)
-        cv2.wait(1)==13
+        cv2.waitKey(1)==13
      ids-np.array(ids)
 
      #================= Train the classifier and save ===================
